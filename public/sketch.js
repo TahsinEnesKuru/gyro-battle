aa78fc19c4fcfc29a53cf6d961154325707e6725
unknown <teneskuru@icloud.com>
2026-02-06 10:55:45 +0000

    First Commit

---
+++ public/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(15);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
